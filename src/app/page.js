3f052b6c201c5d48ab99d6f861f63512a11be35e
RaghuRajMathur <raghurajwe@gmail.com>
2025-12-16 19:19:39 +0530

    feat : minor fix

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import SkillsSphereSection from '../components/SkillsSphereSection';
 import Education from '../components/Education';
 import Experience from '../components/Experience';
 import Projects from '../components/Projects';
-import ContactForm from '../components/Contactform';
+import ContactForm from '../components/ContactForm';
 import Footer from '../components/Footer';
 
 export default function Home() {
